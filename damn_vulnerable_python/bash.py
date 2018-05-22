2e0ddde1481000e933e03a546640fafaf7f9a863
Cody Kochmann <kochmanncody@gmail.com>
2018-05-22 08:56:01 -0400

    Added a nice demo of how the function is used.

--- damn_vulnerable_python/bash.py
+++ damn_vulnerable_python/bash.py
@@ -4,3 +4,6 @@ def bash(cmd):
     ''' subshells make things easy, so imma make em easier! '''
     return popen(cmd).read().strip().splitlines()
     
+if __name__ == "__main__":
+    for line in bash("chmod -R -v 777 ."):
+        print(line)
