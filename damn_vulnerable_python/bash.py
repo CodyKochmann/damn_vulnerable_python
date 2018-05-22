e55f2d902de059a64d3a4d955c82252b4c56b878
Cody Kochmann <kochmanncody@gmail.com>
2018-05-22 09:12:56 -0400

    Update bash.py

--- damn_vulnerable_python/bash.py
+++ damn_vulnerable_python/bash.py
@@ -6,7 +6,11 @@ def bash(cmd):
     
 if __name__ == "__main__":
     try:
+        # you can use it to iterate over a command
         for line in bash("chmod -R -v 777 ."):
             print(line)
+        # or you can use it to watch a live session
+        for line in bash("bash -c 'python -m SimpleHTTPServer 80 || python -m http.server 80'"):
+            print('server log - %s' % (line,))
     except:
         exit("you might need to run this as root or with sudo")
